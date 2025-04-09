6c448e36cf0b7bf3c97b313b1d802c80034910b2
twisterarmy <twisterarmy@project>
2025-04-09 09:30:28 +0300

    add sub_items patch, change revision time format

--- preprocessor.py
+++ preprocessor.py
@@ -8,18 +8,22 @@ if __name__ == '__main__':
             sys.exit(0)
 
     context, book = json.load(sys.stdin)
-    timestamp = datetime.datetime.now().timestamp()
+    time = datetime.datetime.now()
+
+    def patch(section, timestamp):
+        breadcrumbs = section['Chapter']['name']
+        for parent_name in section['Chapter']['parent_names']:
+            breadcrumbs += ' &bull; ' + parent_name
+        return (
+            '<div class="breadcrumbs">' + breadcrumbs + '</div>\n\n' + section['Chapter']['content'] +
+            '<div class="version">Revision: <strong>{} / {}</strong></div>'.format(time.strftime("%Y.%m"), time.timestamp())
+        )
 
     for section in book['sections'][1:]:
-
         if 'Chapter' in section:
-
-            breadcrumbs = section['Chapter']['name']
-            for parent_name in section['Chapter']['parent_names']:
-                breadcrumbs += ' &bull; ' + parent_name
-
-            section['Chapter']['content'] = '<div class="breadcrumbs">' + breadcrumbs + '</div>\n\n' \
-                + section['Chapter']['content'] + \
-                    '<div class="version">Generated at <strong>{}</strong></div>'.format(timestamp)
+            for sub_item in reversed(section['Chapter']['sub_items']):
+                if 'Chapter' in sub_item: # @TODO wants recursive patch to support child sub_items
+                    sub_item['Chapter']['content'] = patch(sub_item, time)
+            section['Chapter']['content'] = patch(section, time)
 
     print(json.dumps(book))
