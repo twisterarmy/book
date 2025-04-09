9f99b88d00019d2d817d34f6610b1a9b88a90cf9
twisterarmy <twisterarmy@project>
2025-04-09 09:40:41 +0300

    fix breadcrumb order

--- preprocessor.py
+++ preprocessor.py
@@ -11,17 +11,18 @@ if __name__ == '__main__':
     time = datetime.datetime.now()
 
     def patch(section, timestamp):
-        breadcrumbs = section['Chapter']['name']
+        breadcrumbs = []
         for parent_name in section['Chapter']['parent_names']:
-            breadcrumbs += ' &bull; ' + parent_name
+            breadcrumbs.append(parent_name)
+        breadcrumbs.append(section['Chapter']['name'])
         return (
-            '<div class="breadcrumbs">' + breadcrumbs + '</div>\n\n' + section['Chapter']['content'] +
+            '<div class="breadcrumbs">' + ' &bull; '.join(breadcrumbs) + '</div>\n\n' + section['Chapter']['content'] +
             '<div class="version">Revision: <strong>{} / {}</strong></div>'.format(time.strftime("%Y.%m"), time.timestamp())
         )
 
     for section in book['sections'][1:]:
         if 'Chapter' in section:
-            for sub_item in reversed(section['Chapter']['sub_items']):
+            for sub_item in section['Chapter']['sub_items']:
                 if 'Chapter' in sub_item: # @TODO wants recursive patch to support child sub_items
                     sub_item['Chapter']['content'] = patch(sub_item, time)
             section['Chapter']['content'] = patch(section, time)
