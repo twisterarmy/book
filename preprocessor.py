b99b9a75cc6968a5817e120c6aa7d20c40f89a59
twisterarmy <twisterarmy@project>
2025-04-10 20:45:06 +0300

    update version format (to skip extra commits before initial release)

--- preprocessor.py
+++ preprocessor.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
             sys.exit(0)
 
     context, book = json.load(sys.stdin)
-    time = datetime.datetime.now()
+    time = datetime.datetime.now().strftime("%Y.%m")
 
     # appends the breadcrumbs version to the content header
     def breadcrumbs(section):
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     # appends the timestamp version to the content footer
     def version(section, timestamp):
-        return section['Chapter']['content'] + '\n\n<div class="version">Generated at: <strong>{} / {}</strong></div>'.format(time.strftime("%Y.%m"), time.timestamp())
+        return section['Chapter']['content'] + '\n\n<div class="version">Generated at: <strong>{}</strong></div>'.format(time)
 
     # apply `version` for the first section
     book['sections'][0]['Chapter']['content'] = version(book['sections'][0], time)
